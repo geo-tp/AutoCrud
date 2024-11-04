745cc6388daca90810b417974769661105508726
Geo <tp53.geo@gmail.com>
2024-11-04 02:22:40 +0100

    add test profile to App

--- src/test/java/com/autocrud/main/AutocrudApplicationTests.java
+++ src/test/java/com/autocrud/main/AutocrudApplicationTests.java
@@ -2,12 +2,13 @@ package com.autocrud.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Active le profil de test
 class AutocrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
