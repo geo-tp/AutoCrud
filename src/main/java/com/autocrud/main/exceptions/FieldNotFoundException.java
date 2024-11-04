03597c44d37a270e976221abb8fd1de05936f9b8
Geo <tp53.geo@gmail.com>
2024-11-04 02:24:14 +0100

    rm old exceptions

--- src/main/java/com/autocrud/main/exceptions/FieldNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.autocrud.main.exceptions;
-
-public class FieldNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public FieldNotFoundException(Long fieldId) {
-        super("Field not found with ID: " + fieldId);
-    }
-}
