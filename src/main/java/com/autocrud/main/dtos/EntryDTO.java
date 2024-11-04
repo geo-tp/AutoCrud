031c34b52238fc4e5f46f21888426f02591561c8
Geo <tp53.geo@gmail.com>
2024-11-04 01:59:05 +0100

    adjust entryDTO

--- src/main/java/com/autocrud/main/dtos/EntryDTO.java
+++ src/main/java/com/autocrud/main/dtos/EntryDTO.java
@@ -1,19 +1,35 @@
 package com.autocrud.main.dtos;
 
+import java.time.LocalDateTime;
+
 public class EntryDTO {
     private Long id;
     private Long fieldId;
     private String value;
+    private Long userId;
+    private LocalDateTime createdAt;
 
     public EntryDTO() {}
 
-    public EntryDTO(Long id, Long fieldId, String value) {
+    public EntryDTO(Long id, Long fieldId, String value, Long userId) {
         this.id = id;
         this.fieldId = fieldId;
         this.value = value;
+        this.userId = userId;
     }
 
-    public EntryDTO(Long fieldId, String value) {
+    public EntryDTO(Long fieldId) {
+        this.fieldId = fieldId;
+    }
+
+    public EntryDTO(Long id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+
+    public EntryDTO(Long id, Long fieldId, String value) {
+        this.id = id;
         this.fieldId = fieldId;
         this.value = value;
     }
@@ -40,5 +56,21 @@ public class EntryDTO {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    public Long getuserId() {
+        return userId;
+    }
+
+    public void setuserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
